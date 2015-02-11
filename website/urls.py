9071819789ad6106e282b105ff9f424018ea3815
PeterisFridvalds <peterisf@inbox.lv>
2015-02-11 13:03:55 +0200

    2015.02.11. 13:00
    ievadforms ar csrf, nevar izvadīt tikko ievadīto

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,5 @@ from website import views
 
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
+        url(r'^show/$', views.show, name='show'),
                        )
--- website/views.py
+++ website/views.py
@@ -15,3 +15,16 @@ def index(request):
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render_to_response('index.html', context_dict, context)
+
+def show(request):
+    # Get the context from the request
+    context = RequestContext(request)
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key content is the same as {{ content }} in the template!
+    context_dict = {'content':"content"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render_to_response('show.html', context_dict, context)
