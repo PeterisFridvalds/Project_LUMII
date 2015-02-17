c74ce854a5d3a0bec54d09f7be1772114aab3493
PeterisFridvalds <peterisf@inbox.lv>
2015-02-17 10:05:37 +0200

    2015.02.17. 10:05
    Pareizi nolasa un izvada nolasīto uz ekrāna

--- website/views.py
+++ website/views.py
@@ -7,24 +7,18 @@ def index(request):
     # Get the context from the request
     context = RequestContext(request)
 
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key content is the same as {{ content }} in the template!
-    context_dict = {'content':"content"}
-
     # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render_to_response('index.html', context_dict, context)
+    return render_to_response('index.html', context)
 
 def show(request):
     # Get the context from the request
     context = RequestContext(request)
-
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key content is the same as {{ content }} in the template!
-    context_dict = {'content':"content"}
+    
+    # Check if it is POST method
+    if request.method == 'POST':
+        # Put input_word in to content
+        context_dict = {'content':request.POST['input_word']}
 
     # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
     return render_to_response('show.html', context_dict, context)
