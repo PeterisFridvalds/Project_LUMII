4239275ed78b0078ca447b63e2117120ca924960
PeterisFridvalds <peterisf@inbox.lv>
2015-02-18 14:48:51 +0200

    2015.02.18. 14:48
    Parāda uz ekrānā visu par šķirkl vienā lielā blokā

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 def index(request):
     # Get the context from the request
@@ -17,8 +18,11 @@ def show(request):
     
     # Check if it is POST method
     if request.method == 'POST':
+        # = open('G:\Fleši\Prakse\Tezaura datu paraugi\test_fails(10r).json', 'r')
+        with open('test_fails(10r).json', encoding='utf-8') as f:
+            document = json.load(f)
         # Put input_word in to content
-        context_dict = {'content':request.POST['input_word']}
+        context_dict = {'content':request.POST['input_word'], 'data':document}
 
     # Return a rendered response to send to the client.
     return render_to_response('show.html', context_dict, context)
